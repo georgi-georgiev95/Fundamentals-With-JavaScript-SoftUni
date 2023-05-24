6577a5581447d5069416cc35aa2232069a262f7e
Georgi957 <gesh007@abv.bg>
2023-05-24 17:42:49 +0300

    Updated Arrays - More Exercise\04.Non-DecreasingSubset

--- 11. Arrays - More Exercise/04.Non-DecreasingSubset.js
+++ 11. Arrays - More Exercise/04.Non-DecreasingSubset.js
@@ -1,32 +1,19 @@
-function nonDecreasingSubsed(arr) {
-    let result = "";
-    let lastBiggestElement = Number.MIN_SAFE_INTEGER;
-    for (let i = 0; i < arr.length; i++) {
-        if (i === 0) {
-            result += arr[i] + " ";
+function nonDecreasingSubsed(input) {
+    let result = [];
+    let maxNum = Number.MIN_SAFE_INTEGER;
+    for (let index = 0; index < input.length; index++) {
+        if (index === 0) {
+            maxNum = Number(input[index]);
+            result.push(maxNum);
         } else {
-            if (lastBiggestElement !== Number.MIN_SAFE_INTEGER) {
-                previousElement = lastBiggestElement;
-                currentElement = Number(arr[i]);
-                if (currentElement >= previousElement) {
-                    result += currentElement + " ";
-                } else {
-                    lastBiggestElement = previousElement;
-                    continue;
-                }
-            } else {
-                let previousElement = Number(arr[i - 1]);
-                let currentElement = Number(arr[i]);
-                if (currentElement >= previousElement) {
-                    result += currentElement + " ";
-                } else {
-                    lastBiggestElement = previousElement;
-                    continue;
-                }
+            let currentNum = Number(input[index]);
+            if (currentNum >= maxNum) {
+                result.push(currentNum);
+                maxNum = currentNum;
             }
         }
     }
-    console.log(result)
+    console.log(result.join(' '));
 }
 nonDecreasingSubsed([ 1, 3, 8, 4, 10, 12, 3, 2, 24]);
 nonDecreasingSubsed([ 1, 2, 3, 4]);
