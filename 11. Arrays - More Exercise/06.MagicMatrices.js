2f0b8f5a873f60154a9aa2e9614cba6dda91be62
Georgi957 <gesh007@abv.bg>
2023-05-24 18:25:56 +0300

    New solution Arrays-More Exercise\06.MagicMatrices

--- 11. Arrays - More Exercise/06.MagicMatrices.js
+++ 11. Arrays - More Exercise/06.MagicMatrices.js
@@ -1,31 +1,43 @@
-function magicMatrice(matrix) {
-    let rowTotal = matrix[0].reduce((a, b) => a + b);
-    let colTotal = 0;
-
-    matrix.forEach(row => {
-        colTotal += row[0];
-    });
-
-    let result = true;
-    let counter = 0;
-
-    for (let i = 0; i < matrix.length; i++) {
-        const currentRow = matrix[i].reduce((acc, item) => acc + item);
-        let currentCol = 0;
-        for (let j = 0; j < matrix.length; j++) {
-            const num = matrix[j][i];
-            currentCol += num;
+function magixMatrix(matrix) {
+    let rowSum = 0;
+    let isMagic = true;
+    for (let array = 0; array < matrix.length; array++) {
+        if (array === 0) {
+            for (let index = 0; index < matrix[array].length; index++) {
+                rowSum += Number(matrix[array][index]);
+            }
+        } else {
+            let nextSum = 0;
+            for (let index = 0; index < matrix[array].length; index++) {
+                nextSum += Number(matrix[array][index]);
+            }
+            if (nextSum !== rowSum) {
+                isMagic = false;
+                break;
+            }
         }
-        if (currentRow !== rowTotal || currentCol !== colTotal) {
-            result = false;
-          	break;
-        }
-
     }
 
-    console.log(result);
+    let colSum = 0;
+    for (let x = 0; x < matrix.length; x++) {
+        if (x === 0) {
+            for (let y = 0; y < matrix.length; y++) {
+                colSum += matrix[y][x];
+            }
+        } else {
+            let nextSum = 0;
+            for (let y = 0; y < matrix.length; y++) {
+                nextSum += matrix[y][x];
+            }
+            if (nextSum !== colSum) {
+                isMagic = false;
+                break;
+            }
+        }
+    }
+    console.log(isMagic);
 }
-magicMatrice([    // true;
+magixMatrix([    // true;
   [4, 5, 6],
   [6, 5, 4],
   [5, 5, 5],
