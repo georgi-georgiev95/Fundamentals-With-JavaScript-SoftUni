b8d0a0b0f0ce7374d8aca1b6aaf3edbee0ed1b1a
Georgi957 <gesh007@abv.bg>
2023-06-04 09:19:13 +0300

    Update 04.radioCrystals.js

--- 15. Functions - More Exercise/04.radioCrystals.js
+++ 15. Functions - More Exercise/04.radioCrystals.js
@@ -1,92 +1,85 @@
-function crystalThickness(arr) {
-  let desiredThickness = arr[0];
+function radioCrystals(input) {
+  let desiredThickness = input[0];
 
-  function cut(chunkSize) {
-    let counter = 0; 
-    while ((chunkSize / 4) >= desiredThickness) {
-      chunkSize = chunkSize / 4;
-      counter++  
-    }
-  
-    return counter;
-  
-  }
-
-  function lap(chunkSize) {
-    let counter = 0;
-    while ((chunkSize*0.8) >= desiredThickness) {
-      chunkSize -= chunkSize * 0.2;
-      counter++;
-    }
-    
-    return counter;
-  }
-
-  function grind(chunkSize) {
-    let counter = 0;
-    while ((chunkSize - 20)>= desiredThickness) {
-      chunkSize -= 20;
-      counter++;
+  for (let index = 1; index < input.length; index++) {
+    let currentCrystal = input[index];
+    console.log(`Processing chunk ${currentCrystal} microns`);
+    if (currentCrystal / 4 >= desiredThickness) {
+      currentCrystal = cut(currentCrystal);
+      currentCrystal = transporting(currentCrystal);
     }
 
-    return counter;
-  }
-
-  function etch(chunkSize) {
-    let counter = 0;
-    while ((chunkSize -2) >= desiredThickness - 1) {
-      chunkSize -= 2;
-      counter++;
-
+    if ((currentCrystal - (currentCrystal / 5)) >= desiredThickness) {
+      currentCrystal = lap(currentCrystal);
+      currentCrystal = transporting(currentCrystal);
     }
 
-    return counter;
-  }
-
-  for (let crystals = 1; crystals < arr.length; crystals++) {
-    let currentChunk = arr[crystals];
-    console.log(`Processing chunk ${currentChunk} microns`);
-
-    let cutCount = cut(currentChunk);
-    if (cutCount > 0) {
-      console.log(`Cut x${cutCount}`);
-      console.log('Transporting and washing');
-      currentChunk = Math.floor(currentChunk / (4 ** cutCount));
+    if ((currentCrystal - 20) >= desiredThickness) {
+      currentCrystal = grind(currentCrystal);
+      currentCrystal = transporting(currentCrystal);
     }
 
-    let lapCount = lap(currentChunk);
-    if (lapCount > 0) {
-      console.log(`Lap x${lapCount}`);
-      console.log('Transporting and washing');
-      for (let lapCounter = 0; lapCounter < lapCount; lapCounter++){
-        currentChunk -= currentChunk * 0.2;
+    if ((currentCrystal - 2) >= (desiredThickness - 1)) {
+      currentCrystal = etch(currentCrystal);
+      currentCrystal = transporting(currentCrystal);
+    }
+
+    if (currentCrystal === desiredThickness - 1) {
+      currentCrystal = xray(currentCrystal);
+    }
+    console.log(`Finished crystal ${currentCrystal} microns`)
+
+    function cut(currentCrystal) {
+      let counter = 0;
+      while ((currentCrystal / 4) >= desiredThickness) {
+        counter++;
+        currentCrystal = currentCrystal / 4;
       }
-      currentChunk = Math.floor(currentChunk);
-    }
-    
-    let grindCount = grind(currentChunk);
-    if (grindCount > 0) {
-      console.log(`Grind x${grindCount}`);
-      console.log('Transporting and washing');
-      currentChunk = Math.floor(currentChunk - 20 * grindCount);
+      console.log(`Cut x${counter}`);
+      return currentCrystal;
     }
 
-    let etchCount = etch(currentChunk);
-    if (etchCount > 0) {
-      console.log(`Etch x${etchCount}`);
-      console.log('Transporting and washing');
-      currentChunk = Math.floor(currentChunk - 2 * etchCount);
+    function transporting(currentCrystal) {
+      console.log("Transporting and washing");
+      return Math.floor(currentCrystal);
     }
 
-    if (currentChunk === desiredThickness - 1) {
-      console.log('X-ray x1');
-      currentChunk++;
+    function lap(currentCrystal) {
+      let counter = 0;
+      while ((currentCrystal - (currentCrystal / 5)) >= desiredThickness) {
+        counter++;
+        currentCrystal -= currentCrystal / 5;
+      }
+      console.log(`Lap x${counter}`);
+      return currentCrystal;
     }
 
-    console.log(`Finished crystal ${currentChunk} microns`);
+    function grind(currentCrystal) {
+      let counter = 0;
+      while ((currentCrystal - 20) >= desiredThickness) {
+        counter++;
+        currentCrystal -= 20;
+      }
+      console.log(`Grind x${counter}`);
+      return currentCrystal;
+    }
+
+    function etch(currentCrystal) {
+      let counter = 0;
+      while ((currentCrystal - 2) >= (desiredThickness - 1)) {
+        counter++;
+        currentCrystal -= 2;
+      }
+      console.log(`Etch x${counter}`);
+      return currentCrystal;
+    }
+
+    function xray(currentCrystal) {
+      console.log("X-ray x1")
+      return currentCrystal + 1;
+    }
   }
 }
-//crystalThickness([1375, 50000]);
-crystalThickness([1000, 4000, 8100]);
+radioCrystals([1000, 4000, 8100])
 
 
