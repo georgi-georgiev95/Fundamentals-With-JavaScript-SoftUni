b60c156a2f1f89b5582be1d96b581338a5c98094
Georgi957 <gesh007@abv.bg>
2023-06-06 11:39:05 +0300

    update

--- 20.Objects and Classes - Lab/songs.js
+++ 20.Objects and Classes - Lab/songs.js
@@ -1,43 +1,35 @@
-function playList(inputArray) {
-    
-    class Song{
+function solve(input) {
+    class Song {
         constructor(typeList, name, time) {
             this.typeList = typeList;
             this.name = name;
             this.time = time;
         }
     }
-
-    let tracksCount = Number(inputArray.shift());
-    let command = inputArray.pop();
-
-    for (const track of inputArray) {
-        let [typeList, name, time] = track.split('_');
-
+    let array = [];
+    let songsCount = Number(input[0]);
+    for (let i = 1; i <= songsCount; i++) {
+        let [typeList, name, time] = input[i].split('_');
         let song = new Song(typeList, name, time);
-
-        if (command === 'all') {
-            console.log(song.name);
-        } else {
-            if (song.typeList === command) {
-                console.log(song.name);
-            }
-        }
+        array.push(song);
     }
-
+    let typeToPrint = input[songsCount + 1];
+    if (typeToPrint === 'all') {
+        array.forEach(object => {
+            console.log(object.name);
+        })
+    } else {
+        array.forEach(object => {
+            if (object.typeList === typeToPrint) {
+                console.log(object.name);
+            }
+        })
+    }
 }
-playList([
-  3,
-  "favourite_DownTown_3:14",
-  "favourite_Kiss_4:16",
-  "favourite_Smooth Criminal_4:01",
-  "favourite",
-]);
-playList([
-  4,
-  "favourite_DownTown_3:14",
-  "listenLater_Andalouse_3:24",
-  "favourite_In To The Night_3:58",
-  "favourite_Live It Up_3:48",
-  "listenLater",
-]);
+solve([4,
+    'favourite_DownTown_3:14',
+    'listenLater_Andalouse_3:24',
+    'favourite_In To The Night_3:58',
+    'favourite_Live It Up_3:48',
+    'listenLater']
+)
