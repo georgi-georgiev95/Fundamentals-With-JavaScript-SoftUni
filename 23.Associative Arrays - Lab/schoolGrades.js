861d7cd2645c2c8614617576db32fe43cc1551af
Georgi957 <gesh007@abv.bg>
2023-06-07 10:47:14 +0300

    Update schoolGrades.js

--- 23.Associative Arrays - Lab/schoolGrades.js
+++ 23.Associative Arrays - Lab/schoolGrades.js
@@ -1,32 +1,27 @@
-function avgGrades(data) {
-    let info = new Map();
-    data.forEach(line => {
-        let name = line.split(' ')[0];
-        let grades = line.split(name + ' ')[1].split(' ').toString();
-        if (info.has(name)) {
-            let currentGrades = info.get(name);
-            let newGrades = currentGrades +','+ grades;
-            info.set(name, newGrades);
-        } else {
-            info.set(name, grades);
+function solve(input) {
+    let assocArr = {};
+    for (const line of input) {
+        let name = '';
+        for (let i = 0; i < line.length; i++) {
+            if (line[i] !== ' ') {
+                name += line[i];
+            } else {
+                break;
+            }
         }
-    })
-    let myArr = [];
-    for (const kvp of info) {
-        let currentGrades = kvp[1].split(',').map(x => Number(x));
-        let count = currentGrades.length;
-        let sum = currentGrades.reduce((a, b) => a + b);
-        let average = (sum / count).toFixed(2);
-        myArr.push([kvp[0], average]);
-        
+        let grades = line.split(name + ' ')[1];
+        if (assocArr.hasOwnProperty(name)) {
+            assocArr[name] += ` ${grades}`;
+        } else {
+            assocArr[name] = grades;
+        }
     }
-    myArr.sort();
-    for (const kvp of myArr) {
-        console.log(`${kvp[0]}: ${kvp[1]}`);
+    const sortedNames = Object.keys(assocArr).sort();
+
+    for (const name of sortedNames) {
+        let gradesArray = assocArr[name].split(' ').map(Number);
+        let gradesCount = gradesArray.length;
+        let averageGrade = (gradesArray.reduce((a, b) => a + b) / gradesCount).toFixed(2);
+        console.log(`${name}: ${averageGrade}`);
     }
 }
-avgGrades(['Lilly 4 6 6 5',
-    'Tim 5 6',
-    'Tammy 2 4 3',
-    'Tim 6 6']
-)
